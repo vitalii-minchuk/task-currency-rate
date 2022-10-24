6ebee010bac21f0daa45f9830cdd006d65ba509e
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-10-24 13:22:51 +0300

    first commit

---
+++ src/app/currency-rate/currency-rate.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-rate',
+  templateUrl: './currency-rate.component.html',
+})
+export class CurrencyRateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
